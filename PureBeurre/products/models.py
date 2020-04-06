9b8333ba66485a79d08e2ff1200e46d53691106d
Geoffrey Remacle <geoffreyremacle@outlook.fr>
2020-04-06 15:09:46 +0200

    création du modèle product

--- PureBeurre/products/models.py
+++ PureBeurre/products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    barcode = models.CharField(max_length=20, primary_key=True)
+    product_name = models.CharField(max_length=80)
+    brand = models.CharField(max_length=80)
+    url = models.URLField()
+    nutrition_grade = models.CharField(max_length=1)
+    nutrition_score = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.product_name
